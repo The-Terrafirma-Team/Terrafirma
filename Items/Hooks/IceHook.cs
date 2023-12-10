5b200b2ff08033142c9c4f60c2a7fb594ec8fdaf
Fred-Leon Brrandibur <fredleon11@gmail.com>
2023-12-10 03:15:33 +0100

    Ice Hook and some other changes

--- Items/Hooks/IceHook.cs
+++ Items/Hooks/IceHook.cs
@@ -1,11 +1,5 @@
-// DO NOT USE THIS SHIT IT"S BAD AND VERY BAD AND EVIL AND IT WILL EAT ALL YOUR BREADCRUMBS AND BEDBUGS.
-//Pls Fix Jeo :3 :3
-
-
-using Microsoft.CodeAnalysis;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using ReLogic.Content;
 using Terraria;
 using Terraria.ID;
@@ -13,28 +7,26 @@ using Terraria.ModLoader;
 
 namespace TerrafirmaRedux.Items.Hooks
 {
-	internal class SingularityHook : ModItem
+	internal class IceHook : ModItem
 	{
 		public override void SetDefaults() {
 			Item.CloneDefaults(ItemID.AmethystHook);
 			Item.shootSpeed = 18f;
-			Item.shoot = ModContent.ProjectileType<SingularityHookProjectile>();
+			Item.shoot = ModContent.ProjectileType<IceHookProjectile>();
 
 		}
 
 	}
 
-	internal class SingularityHookProjectile : ModProjectile
+	internal class IceHookProjectile : ModProjectile
 	{
- 
-		Vector2 MousePos = Vector2.Zero;
 		private static Asset<Texture2D> chainTexture;
 
 		public override void Load() {
-			chainTexture = ModContent.Request<Texture2D>("TerrafirmaRedux/Items/Hooks/SingularityHookChain");
+			chainTexture = ModContent.Request<Texture2D>("TerrafirmaRedux/Items/Hooks/IceHookChain");
 		}
 
-        public override void Unload() { // This is called once on mod reload when this piece of content is being unloaded.
+		public override void Unload() { // This is called once on mod reload when this piece of content is being unloaded.
 			// It's currently pretty important to unload your static fields like this, to avoid having parts of your mod remain in memory when it's been unloaded.
 			chainTexture = null;
 		}
@@ -43,54 +35,22 @@ namespace TerrafirmaRedux.Items.Hooks
 			ProjectileID.Sets.SingleGrappleHook[Type] = true;
 		}
 
-        public override void AI()
-        {
-			if (Projectile.ai[1] == 0) 
-			{
-			Projectile.ai[1] = 1;
-			MousePos = Main.MouseWorld;
-			}
-
-            base.AI();
-        }
-        public override void UseGrapple(Player player, ref int type)
-        {
-            base.UseGrapple(player, ref type);
-        }
-
-        public override void SetDefaults() {
+		public override void SetDefaults() {
 			Projectile.CloneDefaults(ProjectileID.GemHookAmethyst); // Copies the attributes of the Amethyst hook's projectile.
 		}
 
-        // Use this hook for hooks that can have multiple hooks mid-flight: Dual Hook, Web Slinger, Fish Hook, Static Hook, Lunar Hook.
+		// Use this hook for hooks that can have multiple hooks mid-flight: Dual Hook, Web Slinger, Fish Hook, Static Hook, Lunar Hook.
         public override float GrappleRange() {
-			return 400f;
+			return 240f;
 		}
-
-        public override bool? GrappleCanLatchOnTo(Player player, int x, int y) {
-			// By default, the hook returns null to apply the vanilla conditions for the given tile position (this tile position could be air or an actuated tile!)
-			// If you want to return true here, make sure to check for Main.tile[x, y].HasUnactuatedTile (and Main.tileSolid[Main.tile[x, y].TileType] and/or Main.tile[x, y].HasTile if needed)
-
-			// We make this hook latch onto trees just like Squirrel Hook
-
-			// Tree trunks cannot be actuated so we don't need to check for that here
-			Tile tile = Main.tile[x, y];
-			if (Projectile.active && Projectile.Distance(Main.player[Projectile.owner].Center) >= GrappleRange() || Projectile.active &&  Projectile.position.Distance(MousePos) < 5f ) {
-				return true;
-			}
-
-			// In any other case, behave like a normal hook
-			return null;
-		}
-		
 
         public override void GrappleRetreatSpeed(Player player, ref float speed)
         {
-            speed = 20f;
+            speed = 24f;
         }
 
         public override void GrapplePullSpeed(Player player, ref float speed) {
-			speed = 18;
+			speed = 25;
 		}
 
         public override bool PreDrawExtras() {
