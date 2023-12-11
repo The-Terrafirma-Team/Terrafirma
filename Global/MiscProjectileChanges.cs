5d755a3874179e6e45bb9542d1efc1fa6ef05ea3
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-12-11 00:02:21 -0600

    wand of poisoning

---
+++ Global/MiscProjectileChanges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Global
+{
+    public class MiscProjectileChanges : GlobalProjectile
+    {
+        public override void SetDefaults(Projectile entity)
+        {
+            if(entity.type == ProjectileID.ThrowingKnife || entity.type == ProjectileID.PoisonedKnife)
+            {
+                entity.usesLocalNPCImmunity = true;
+                entity.localNPCHitCooldown = -1;
+            }
+        }
+    }
+}
--- Items/Weapons/Ranged/KnifeShooter.cs
+++ Items/Weapons/Ranged/KnifeShooter.cs
@@ -20,10 +20,10 @@ namespace TerrafirmaRedux.Items.Weapons.Ranged
             Item.knockBack = 2f;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.useAnimation = 8;
-            Item.useTime = 5;
+            Item.useTime = 8;
             Item.width = 44;
             Item.height = 22;
-            Item.UseSound = SoundID.Item1;
+            Item.UseSound = SoundID.Item11;
             Item.DamageType = DamageClass.Ranged;
             Item.autoReuse = true;
             Item.noMelee = true;
@@ -35,16 +35,19 @@ namespace TerrafirmaRedux.Items.Weapons.Ranged
             Item.shoot = ProjectileID.ThrowingKnife;
             Item.shootSpeed = 12f;
         }
-
+        public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            return !Main.rand.NextBool(3);
+        }
         public override Vector2? HoldoutOffset()
         {
             return new Vector2(0,0);
         }
-
         public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
             Vector2 muzzleoff = new Vector2(Item.width - 5, 4 * player.direction).RotatedBy(Math.Atan2(velocity.Y,velocity.X));
             position = player.Center + muzzleoff;
+            velocity = velocity.RotatedByRandom(0.01f);
         }
     }
 }
