daf79dca9ba6086934929e72bfeb6f56af39b687
Fred-Leon Brrandibur <fredleon11@gmail.com>
2023-12-11 03:06:14 +0100

    Star Revolver Added

---
+++ Items/Weapons/Ranged/StarRevolver.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Items.Weapons.Ranged
+{
+    internal class StarRevolver : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 32;
+            Item.knockBack = 2f;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAnimation = 16;
+            Item.useTime = 16;
+            Item.width = 38;
+            Item.height = 26;
+            Item.UseSound = SoundID.Item9;
+            Item.DamageType = DamageClass.Ranged;
+            Item.autoReuse = false;
+            Item.noMelee = true;
+
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(0, 0, 3, 0);
+
+            Item.useAmmo = AmmoID.FallenStar;
+            Item.shoot = ProjectileID.FallingStar;
+            Item.shootSpeed = 16f;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-1,0);
+        }
+    }
+}
