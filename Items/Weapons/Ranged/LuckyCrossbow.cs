c7db09f69623f09025ab5950042e086d1cbe0268
Fred-Leon Brrandibur <fredleon11@gmail.com>
2023-12-14 03:16:38 +0100

    Lucky Rarity and Semi-Finished Lucky Crossbow

---
+++ Items/Weapons/Ranged/LuckyCrossbow.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using TerrafirmaRedux.Rarities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Items.Weapons.Ranged
+{
+    internal class LuckyCrossbow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 11;
+            Item.knockBack = 2f;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAnimation = 8;
+            Item.useTime = 8;
+            Item.width = 64;
+            Item.height = 34;
+            Item.UseSound = SoundID.Item1;
+            Item.DamageType = DamageClass.Ranged;
+            Item.autoReuse = true;
+            Item.noMelee = true;
+
+            Item.rare = ModContent.RarityType<LuckyRarity>();
+            Item.value = Item.sellPrice(0, 40, 0, 0);
+
+            Item.useAmmo = AmmoID.Arrow;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shootSpeed = 20f;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-7,-4);
+        }
+
+        public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            if (Main.rand.Next(101) <= 33)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Rarities/LuckyRarity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Rarities
+{
+    internal class LuckyRarity : ModRarity
+    {
+        public override Color RarityColor => new Color(255, 217, 10);
+
+        public override int GetPrefixedRarity(int offset, float valueMult)
+        {
+            return Type; // no 'lower' tier to go to, so return the type of this rarity.
+        }
+    }
+}
