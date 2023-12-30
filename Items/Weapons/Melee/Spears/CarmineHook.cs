62fe09c101b83776b8cffe170bd4b6e81f359fc7
SeagullBoii <maenaad@gmail.com>
2023-12-30 04:29:55 +0200

    Carmine Hook

---
+++ Items/Weapons/Melee/Spears/CarmineHook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrafirmaRedux.Projectiles.Melee;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Items.Weapons.Melee.Spears
+{
+    internal class CarmineHook: ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 70;
+            Item.useTime = 26;
+            Item.useAnimation = 26;
+            Item.knockBack = 6;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.DamageType = DamageClass.Melee;
+            Item.UseSound = SoundID.Item1;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+            Item.shoot = ModContent.ProjectileType<CarmineHookProjectile>();
+            Item.shootSpeed = 10;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DarkLance)
+                .AddIngredient(ItemID.TheRottedFork)
+                .AddIngredient(ItemID.Ectoplasm, 10)
+                .AddIngredient(ItemID.Ichor, 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
---
+++ Projectiles/Melee/CarmineHookProjectile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using TerrafirmaRedux.Global.Templates;
+
+namespace TerrafirmaRedux.Projectiles.Melee
+{
+    public class CarmineHookProjectile: SpearTemplate
+    {
+        protected override float HoldoutRangeMax => 150;
+        protected override float HoldoutRangeMin => 40;
+
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.Spear);
+            DrawOriginOffsetY = 100;
+        }
+
+        public override void PostAI()
+        {
+            int S = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Ichor);
+            Main.dust[S].noGravity = true;
+            Main.dust[S].velocity = Projectile.velocity * 2;
+            Main.dust[S].fadeIn = Main.rand.NextFloat(0, 1.5f);
+            int H = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.IchorTorch);
+            Main.dust[H].noGravity = true;
+            Main.dust[H].velocity = Projectile.velocity * -3;
+            Main.dust[H].fadeIn = Main.rand.NextFloat(0, 1.5f);
+
+            Player player = Main.player[Projectile.owner];
+            if (Projectile.timeLeft == (int)(player.itemAnimationMax * 1.3f) && Main.LocalPlayer == player)
+            {
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity * 2.5f,
+                                        ProjectileID.IchorSplash, Projectile.damage, Projectile.knockBack, Projectile.owner);
+            }
+        }
+    }
+}
