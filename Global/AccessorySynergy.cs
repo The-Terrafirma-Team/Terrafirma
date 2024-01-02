4cb9b194e9d7dc5a37ec40fc06c1898007a62c4b
Fred-Leon Brrandibur <fredleon11@gmail.com>
2024-01-02 19:57:09 +0100

    Heart Container

---
+++ Global/AccessorySynergy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Global
+{
+    public class AccessorySynergyPlayer : ModPlayer {
+
+        public int[] EquippedAccessories = new int[] {};
+        string[] ActivatedSynergies = new string[] { };
+
+        public override void ResetEffects()
+        {
+           EquippedAccessories = new int[] {};
+           ActivatedSynergies = new string[] {};
+        }
+
+        public override void UpdateEquips()
+        {
+            //Check For Synergies 
+            if (EquippedAccessories.Contains(ItemID.AngelWings) && EquippedAccessories.Contains(ItemID.HandOfCreation))
+            {
+                ActivatedSynergies = ActivatedSynergies.Append("CoolSynergy").ToArray();
+            }
+
+        }
+
+    }
+
+    public class GlobalAccessorySynergy : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override void UpdateEquip(Item item, Player player)
+        {
+            player.GetModPlayer<AccessorySynergyPlayer>().EquippedAccessories = player.GetModPlayer<AccessorySynergyPlayer>().EquippedAccessories.Append(item.type).ToArray();
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if ( player.GetModPlayer<AccessorySynergyPlayer>() )
+        }
+
+    }
+}
+
+
+
+
