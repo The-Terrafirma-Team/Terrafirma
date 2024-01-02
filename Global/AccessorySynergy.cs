0a0512c8247b3dcea73d50417c510dc9b47011e5
Fred-Leon Brrandibur <fredleon11@gmail.com>
2024-01-02 22:13:22 +0100

    Synergies Part 1

--- Global/AccessorySynergy.cs
+++ Global/AccessorySynergy.cs
@@ -1,49 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using TerrafirmaRedux.Global.Structs;
+using TerrafirmaRedux.Items.Equipment.Ranged;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace TerrafirmaRedux.Global
 {
-    public class AccessorySynergyPlayer : ModPlayer {
+    public class AccessorySynergyPlayer : ModPlayer
+    {
 
-        public int[] EquippedAccessories = new int[] {};
-        string[] ActivatedSynergies = new string[] { };
+        public int[] EquippedAccessories = new int[] { };
+        public SynergyData[] ActivatedSynergies = new SynergyData[] { };
 
         public override void ResetEffects()
         {
-           EquippedAccessories = new int[] {};
-           ActivatedSynergies = new string[] {};
+            EquippedAccessories = new int[] { };
+            ActivatedSynergies = new SynergyData[] { };
         }
 
         public override void UpdateEquips()
         {
             //Check For Synergies 
-            if (EquippedAccessories.Contains(ItemID.AngelWings) && EquippedAccessories.Contains(ItemID.HandOfCreation))
+
+            //Silly Ammo Belt
+            if (EquippedAccessories.Contains(ModContent.ItemType<DrumMag>()) && EquippedAccessories.Contains(ModContent.ItemType<AmmoCan>()) )
             {
-                ActivatedSynergies = ActivatedSynergies.Append("CoolSynergy").ToArray();
+                ActivatedSynergies = ActivatedSynergies.Append(new SynergyData("Silly Ammo Belt", "30% chance for guns to shoot a random bullet", new int[] { ModContent.ItemType<DrumMag>() , ModContent.ItemType<AmmoCan>() })).ToArray();
             }
 
         }
-
     }
 
     public class GlobalAccessorySynergy : GlobalItem
     {
+        Player itemplayer;
         public override bool InstancePerEntity => true;
 
         public override void UpdateEquip(Item item, Player player)
         {
             player.GetModPlayer<AccessorySynergyPlayer>().EquippedAccessories = player.GetModPlayer<AccessorySynergyPlayer>().EquippedAccessories.Append(item.type).ToArray();
+            itemplayer = player;
         }
-
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
-            if ( player.GetModPlayer<AccessorySynergyPlayer>() )
-        }
 
+            SynergyData pickedSynergy = new SynergyData("", "", new int[] { });
+            if (itemplayer != null)
+            {
+                for (int i = 0; i < itemplayer.GetModPlayer<AccessorySynergyPlayer>().ActivatedSynergies.Length; i++)
+                {
+                    if (itemplayer.GetModPlayer<AccessorySynergyPlayer>().ActivatedSynergies[i].Accessories.Contains(item.type))
+                    {
+                        pickedSynergy = itemplayer.GetModPlayer<AccessorySynergyPlayer>().ActivatedSynergies[i];
+                        break;
+                    }
+                }
+            }
+
+
+            if (pickedSynergy.Name != "")
+            {
+                string SynergyAccessories = "";
+                for (int i = 0; i < pickedSynergy.Accessories.Length; i++)
+                {
+                    Item accessory = new Item();
+                    accessory.SetDefaults(pickedSynergy.Accessories[i]);
+
+                    SynergyAccessories += accessory.Name;
+                    if (i < pickedSynergy.Accessories.Length - 2)
+                    {
+                        SynergyAccessories += ", ";
+                    }
+                    else if (i == pickedSynergy.Accessories.Length - 2)
+                    {
+                        SynergyAccessories += " and ";
+                    }
+                }
+
+
+                tooltips.Add(new TooltipLine(TerrafirmaRedux.Mod, "SynergyName", "[c/2BE5FF:" + pickedSynergy.Name + "]" ));
+                tooltips.Add(new TooltipLine(TerrafirmaRedux.Mod, "SynergyItems", "[c/2BE5FF:" + "Synergy with " + SynergyAccessories + "]" ));
+                tooltips.Add(new TooltipLine(TerrafirmaRedux.Mod, "SynergyDescription", "[c/2BE5FF:" + pickedSynergy.Description + "]" ));
+
+
+
+            }
+        }
     }
 }
 
---
+++ Global/Structs/SynergyStruct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrafirmaRedux.Global.Structs
+{
+    public struct SynergyData
+    {
+        public string Name;
+        public string Description;
+        public int[] Accessories;
+
+        public SynergyData(string Name , string Description, int[] Accessories)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Accessories = Accessories;
+        }
+    }
+}
