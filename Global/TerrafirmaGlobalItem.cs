393361577b33295ebfb5b873c379d138704fdfb0
Fred-Leon Brrandibur <fredleon11@gmail.com>
2024-01-04 11:57:31 +0100

    Fix bs

--- Global/TerrafirmaGlobalItem.cs
+++ Global/TerrafirmaGlobalItem.cs
@@ -15,13 +15,15 @@ namespace TerrafirmaRedux.Global
 //CanConsumeAmmo
 
         int[] BulletSlots = new int[] { };
-        int canrand;
+        int canrand = 0;
         public override bool CanConsumeAmmo(Item weapon, Item ammo, Player player)
         {
-            
+
+            #region Ammo Can
             //Ammo Can
             if (weapon.useAmmo == AmmoID.Bullet && player.GetModPlayer<TerrafirmaGlobalPlayer>().AmmoCan)
             {
+                BulletSlots = new int[] { };
                 for (int i = 54; i < 58; i++)
                 {
                     if (player.inventory[i].ammo == AmmoID.Bullet)
@@ -30,8 +32,10 @@ namespace TerrafirmaRedux.Global
                         BulletSlots = BulletSlots.Append(i).ToArray();
                     }
                 }
+
                 return false;
-            }
+            } 
+            #endregion
 
             return base.CanConsumeAmmo(weapon, ammo, player);
         }
@@ -40,15 +44,22 @@ namespace TerrafirmaRedux.Global
         public override void ModifyShootStats(Item item, Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
 
+            #region Ammo Can
             //Ammo Can
-            if (item.useAmmo == AmmoID.Bullet && player.GetModPlayer<TerrafirmaGlobalPlayer>().AmmoCan)
+            if (item.useAmmo == AmmoID.Bullet && player.GetModPlayer<TerrafirmaGlobalPlayer>().AmmoCan && BulletSlots.Length != 0)
             {
+                
+
                 canrand = BulletSlots[Main.rand.Next(BulletSlots.Length)];
 
                 type = player.inventory[canrand].shoot;
                 player.inventory[canrand].stack--;
-            }
 
+                BulletSlots = new int[] { };
+            }
+            #endregion
+
+            #region Silly Ammo Belt Synergy
             //Silly Ammo Belt Synergy
             if (player.GetModPlayer<AccessorySynergyPlayer>().ActivatedSynergyNames() != null)
             {
@@ -56,13 +67,14 @@ namespace TerrafirmaRedux.Global
                 {
                     if (Main.rand.Next(100) < 30)
                     {
-                        Item newitem = new Item(TerrafirmaRedux.Mod.BulletArray[Main.rand.Next(TerrafirmaRedux.Mod.BulletArray.Length)] );
+                        Item newitem = new Item(TerrafirmaRedux.Mod.BulletArray[Main.rand.Next(TerrafirmaRedux.Mod.BulletArray.Length)]);
                         type = newitem.shoot;
                     }
                 }
             }
 
-            base.ModifyShootStats(item, player, ref position, ref velocity, ref type, ref damage, ref knockback);
+            base.ModifyShootStats(item, player, ref position, ref velocity, ref type, ref damage, ref knockback); 
+            #endregion
         }
     }
 }
