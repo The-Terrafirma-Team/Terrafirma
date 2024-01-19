0e160302ba95e7affe3e6fe5a0a950898ca9088b
Fred-Leon Brrandibur <fredleon11@gmail.com>
2024-01-19 06:39:34 +0100

    Guns

---
+++ Items/Weapons/Ranged/Microshark.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrafirmaRedux.Projectiles.Ranged;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Items.Weapons.Ranged
+{
+    internal class Microshark: ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 60;
+            Item.knockBack = 5f;
+            Item.crit = 15;
+
+            Item.width = 66;
+            Item.height = 33;
+
+            Item.autoReuse = false;
+            Item.useTime = 24;
+            Item.useAnimation = 24;
+
+
+            Item.shoot = ProjectileID.Bullet;
+            Item.useAmmo = AmmoID.Bullet;
+            Item.shootSpeed = 15;
+            Item.UseSound = SoundID.Item11;
+            Item.value = Item.sellPrice(gold: 1);
+
+            Item.rare = ItemRarityID.Orange;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.DamageType = DamageClass.Ranged;
+            Item.scale = 0.9f;
+
+            
+        }
+
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            hitbox.Width = 20;
+            hitbox.Height = 20;
+            hitbox.X = (int)player.MountedCenter.X + (int)new Vector2(60, 0).RotatedBy(player.itemRotation).X * player.direction - 10;
+            hitbox.Y = (int)player.MountedCenter.Y + (int)new Vector2(60, 0).RotatedBy(player.itemRotation).Y * player.direction - 10;
+            base.UseItemHitbox(player, ref hitbox, ref noHitbox);
+        }
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-24, -6);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleoff = new Vector2(Item.width /  2,  0).RotatedBy(Math.Atan2(velocity.Y, velocity.X));
+            position = player.Center + muzzleoff;
+            if (type == ProjectileID.Bullet)
+            {
+                type = ProjectileID.BulletHighVelocity;
+            }
+        }
+    }
+}
---
+++ Projectiles/Ranged/TyphoonBulletProjectile.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrafirmaRedux.Projectiles.Ranged
+{
+    internal class TyphoonBulletProjectile : ModProjectile
+    {
+        public override string Texture => null;
+        public override void SetDefaults()
+        {
+            Projectile.width = 8; 
+            Projectile.height = 8; 
+
+            Projectile.friendly = true; 
+            Projectile.hostile = false; 
+            Projectile.DamageType = DamageClass.Ranged; 
+            Projectile.penetrate = 3; 
+
+            Projectile.timeLeft = 600; 
+
+            Projectile.ignoreWater = true; 
+            Projectile.tileCollide = true; 
+            Projectile.extraUpdates = 1;
+
+            Projectile.aiStyle = 1;
+            AIType = ProjectileID.Bullet;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            Dust newdust = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.Water_Jungle, -Projectile.velocity.X / 5f, -Projectile.velocity.Y / 5f, 0, Color.White, 1f);
+            newdust.noGravity = true;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.Kill();
+            return false;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Main.instance.LoadProjectile(Projectile.type);
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+
+    }
+}
