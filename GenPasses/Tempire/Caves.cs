11f36bef06db8b156c249563bd36aae75aed1195
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-02-13 19:53:55 -0600

    Start of cave gen

---
+++ GenPasses/Tempire/Caves.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using Terraria;
+using Terrafirma.Subworlds.Tempire;
+using Terrafirma.Tiles.Tempire;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace Terrafirma.GenPasses.Tempire
+{
+    public class Caves : GenPass
+    {
+        //TODO: remove this once tML changes generation passes
+        public Caves() : base("Terrain", 1) { }
+
+        public void StarCaves()
+        {
+            FastNoiseLite gennoise = new FastNoiseLite();
+            gennoise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
+            gennoise.SetFrequency(0.02f);
+
+            for (int i = 0; i < TempireSubworld.WorldWidth - 1; i++)
+            {
+                for (int j = (int)(TempireSubworld.WorldHeight / 1.95f); j < TempireSubworld.WorldHeight - 1; j++)
+                {
+                    if (Math.Abs(gennoise.GetNoise(i, j)) < gennoise.GetNoise(j, i) * MathHelper.Clamp((j - TempireSubworld.WorldHeight / 2f) / (float)TempireSubworld.WorldHeight,0f,0.3f))
+                    {
+                        Tile tile = Main.tile[i, j];
+                        tile.HasTile = false;
+                    }
+                }
+            }
+        }
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            progress.Message = "Generating caves"; // Sets the text displayed for this pass
+
+            StarCaves();
+        }
+    }
+}
---
+++ GenUtils.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Terrafirma
+{
+    public class GenUtils
+    {
+        public static void MakeCircle(int x, int y, int radius, int type)
+        {
+            int xmin = x - radius;
+            int xmax = x + radius;
+            int ymin = y - radius;
+            int ymax = y + radius;
+            for (int i = xmin; i < xmax; i++)
+            {
+                for (int j = ymin; j < ymax; j++)
+                {
+                    if (Vector2.Distance(new Vector2(i, j), new Vector2(x, y)) < radius)
+                    {
+                        Tile t = Main.tile[i, j];
+                        t.HasTile = true;
+                        t.TileType = (ushort)type;
+                    }
+                }
+            }
+        }
+        public static void CarveCircle(int x, int y, int radius)
+        {
+            int xmin = x - radius;
+            int xmax = x + radius;
+            int ymin = y - radius;
+            int ymax = y + radius;
+            for (int i = xmin; i < xmax; i++)
+            {
+                for (int j = ymin; j < ymax; j++)
+                {
+                    if (Vector2.Distance(new Vector2(i, j), new Vector2(x, y)) < radius)
+                    {
+                        Tile t = Main.tile[i, j];
+                        t.HasTile = false;
+                    }
+                }
+            }
+        }
+    }
+}
