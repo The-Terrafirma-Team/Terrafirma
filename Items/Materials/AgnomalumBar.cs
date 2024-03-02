9c10fd919efdd405f64b256597254d432c448811
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-03-02 16:09:20 -0600

    Agnomalum Bar

---
+++ Items/Materials/AgnomalumBar.cs
@@ -0,0 +1,23 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terrafirma.Tiles;
+
+namespace Terrafirma.Items.Materials
+{
+    public class AgnomalumBar : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.Size = new Vector2(16);
+            Item.rare = ItemRarityID.Yellow;
+            Item.value = Item.sellPrice(0, 0, 15, 0);
+            Item.DefaultToPlaceableTile(ModContent.TileType<PlacedBars>(), 0);
+        }
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+        }
+    }
+}
---
+++ Tiles/PlacedBars.cs
@@ -0,0 +1,26 @@
+﻿using Terraria.Localization;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+
+namespace Terrafirma.Tiles
+{
+    public class PlacedBars : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.MetalBar")); // localized text for "Metal Bar"
+        }
+    }
+}
