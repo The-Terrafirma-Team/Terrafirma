00bee5a89a89b17fc03776b2af0e46149ea1dbea
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-03-09 19:49:32 -0600

    Update RecipeSystem.cs

--- Global/RecipeSystem.cs
+++ Global/RecipeSystem.cs
@@ -15,18 +15,20 @@ namespace Terrafirma.Global
         {
             for(int i = 0; i < Recipe.numRecipes; i++) 
             {
+                // add souls to hallowed armor
                 Recipe recipe = Main.recipe[i];
-                if(recipe.HasResult(ItemID.HallowedGreaves) || recipe.HasResult(ItemID.HallowedPlateMail) || recipe.HasResult(ItemID.HallowedHelmet) || recipe.HasResult(ItemID.HallowedHeadgear) || recipe.HasResult(ItemID.HallowedHood) || recipe.HasResult(ItemID.HallowedMask) ||
+                if (recipe.HasResult(ItemID.HallowedGreaves) || recipe.HasResult(ItemID.HallowedPlateMail) || recipe.HasResult(ItemID.HallowedHelmet) || recipe.HasResult(ItemID.HallowedHeadgear) || recipe.HasResult(ItemID.HallowedHood) || recipe.HasResult(ItemID.HallowedMask) ||
                     recipe.HasResult(ItemID.AncientHallowedGreaves) || recipe.HasResult(ItemID.AncientHallowedPlateMail) || recipe.HasResult(ItemID.AncientHallowedHelmet) || recipe.HasResult(ItemID.AncientHallowedHeadgear) || recipe.HasResult(ItemID.AncientHallowedHood) || recipe.HasResult(ItemID.AncientHallowedMask))
                 {
-                    recipe.AddIngredient(ItemID.SoulofFright,5);
+                    recipe.AddIngredient(ItemID.SoulofFright, 5);
                     recipe.AddIngredient(ItemID.SoulofSight, 5);
                     recipe.AddIngredient(ItemID.SoulofMight, 5);
 
                 }
-                else if(recipe.HasResult(ItemID.AdamantiteForge) || recipe.HasResult(ItemID.TitaniumForge))
+                // Add majestic gel to adamantite forges
+                else if (recipe.HasResult(ItemID.AdamantiteForge) || recipe.HasResult(ItemID.TitaniumForge))
                 {
-                    recipe.AddIngredient(ModContent.ItemType<MajesticGel>(),10);
+                    recipe.AddIngredient(ModContent.ItemType<MajesticGel>(), 10);
                 }
             }
             #region Uncraftable to Craftable
