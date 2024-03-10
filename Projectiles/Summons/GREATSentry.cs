6f0ab56ed425c220c197d5830d505b52de08aa22
Fred-Leon Brrandibur <fredleon11@gmail.com>
2024-03-10 15:14:47 +0100

    Big Turet

--- Projectiles/Summons/GREATSentry.cs
+++ Projectiles/Summons/GREATSentry.cs
@@ -17,7 +17,6 @@ namespace Terrafirma.Projectiles.Summons
         NPC closestnpc = null;
         public override void SetDefaults()
         {
-            Projectile.damage = 30;
             Projectile.friendly = true;
 
             Projectile.height = 80;
@@ -45,32 +44,87 @@ namespace Terrafirma.Projectiles.Summons
         }
         public override bool? CanHitNPC(NPC target)
         {
-            return false;
+            return Projectile.ai[0] > 180 && Projectile.ai[1] % (int)(10 * TFUtils.GetSentryAttackCooldownMultiplier(Main.projectile[Projectile.whoAmI])) == 0;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float collsionPoint = 0;
+            return Collision.CheckAABBvLineCollision(targetHitbox.Center(), targetHitbox.Size(),
+                    Projectile.TopLeft + new Vector2(37, 45) - new Vector2(0, 12).RotatedBy(Projectile.rotation),
+                    (Projectile.TopLeft + new Vector2(37, 45)) + new Vector2(1800, 0).RotatedBy(Projectile.rotation) - new Vector2(0, 12).RotatedBy(Projectile.rotation),
+                    80,
+                    ref collsionPoint);
         }
         public override void AI()
         {
+            Projectile.damage = (int)(Projectile.originalDamage * (Math.Clamp((Projectile.ai[0] - 120f) / 240f, 0f, 2f) + 1f));
 
-            if (closestnpc != null)
+            if (Projectile.ai[1] % 27 == 0) SoundEngine.PlaySound(SoundID.Item15 with { Volume = Math.Clamp(Projectile.ai[0] / 480f, 0.05f, 1f), IsLooped = false }, Projectile.Center);
+
+            //for (int i = 0; i < 40; i++)
+            //{
+            //    Dust dust = Dust.NewDustPerfect((Projectile.TopLeft + new Vector2(37, 45)) + new Vector2(18 * i, 0).RotatedBy(Projectile.rotation) - new Vector2(0, 12 * Projectile.spriteDirection).RotatedBy(Projectile.rotation), DustID.CrimsonTorch, Vector2.Zero, 1, Color.White, 1.5f);
+            //    dust.noGravity = true;
+            //}
+
+            if (Main.player[Projectile.owner].HasMinionAttackTargetNPC && Projectile.Center.Distance(Main.npc[Main.player[Projectile.owner].MinionAttackTargetNPC].Center) < 600f)
             {
-                Projectile.ai[1] = Utils.AngleLerp(Projectile.ai[1], (Projectile.TopLeft + new Vector2(32, 38)).AngleTo(closestnpc.Center), 0.07f);
-                if (Math.Abs(Projectile.ai[1]) > MathHelper.PiOver2) Projectile.spriteDirection = -1;
+                closestnpc = Main.npc[Main.player[Projectile.owner].MinionAttackTargetNPC];
+
+                Projectile.rotation = Utils.AngleLerp(Projectile.rotation, (Projectile.TopLeft + new Vector2(37, 37)).AngleTo(closestnpc.Center), 0.07f);
+
+                if (Math.Abs(Projectile.rotation) > MathHelper.PiOver2) Projectile.spriteDirection = -1;
                 else Projectile.spriteDirection = 1;
+
                 Projectile.ai[0] = Math.Clamp(Projectile.ai[0] + 1, 0, 600);
             }
-
-            if (closestnpc == null || !closestnpc.active || Projectile.Center.Distance(closestnpc.Center) > 400f)
+            else
             {
-                NPC closestsnpcsearch = TFUtils.FindClosestNPC(400f, Projectile.Center);
-                closestnpc = null;
-                if (closestsnpcsearch != null)
+                if (closestnpc != null)
                 {
-                    closestnpc = closestsnpcsearch;
+                    Projectile.rotation = Utils.AngleLerp(Projectile.rotation, (Projectile.TopLeft + new Vector2(37, 37)).AngleTo(closestnpc.Center), 0.07f);
+
+                    if (Math.Abs(Projectile.rotation) > MathHelper.PiOver2) Projectile.spriteDirection = -1;
+                    else Projectile.spriteDirection = 1;
+
+                    Projectile.ai[0] = Math.Clamp(Projectile.ai[0] + 1, 0, 600);
                 }
-                Projectile.ai[0] = Projectile.ai[0] > 0 ? Projectile.ai[0] - 1 : 0;
-                
+
+                if (closestnpc == null || !closestnpc.active || Projectile.Center.Distance(closestnpc.Center) > 600f)
+                {
+                    NPC closestsnpcsearch = TFUtils.FindClosestNPC(600f, Projectile.Center);
+                    closestnpc = null;
+
+                    if (Math.Abs(Projectile.rotation) > MathHelper.PiOver2)
+                    {
+                        Projectile.rotation = Utils.AngleLerp(Projectile.rotation, (float)Math.PI, 0.01f);
+                        Projectile.spriteDirection = -1;
+                    }
+                    else
+                    {
+                        Projectile.rotation = Utils.AngleLerp(Projectile.rotation, 0f, 0.01f);
+                        Projectile.spriteDirection = 1;
+                    }
+
+                    if (closestsnpcsearch != null)
+                    {
+                        closestnpc = closestsnpcsearch;
+                    }
+                    Projectile.ai[0] = Projectile.ai[0] > 0 ? Projectile.ai[0] - 3 : 0;
+
+                }
             }
+            Projectile.ai[1]++;
             
-                
+            if (Projectile.ai[0] > 120 && Projectile.ai[1] % 4 == 0)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Dust dust = Dust.NewDustPerfect((Projectile.TopLeft + new Vector2(37, 45)) + new Vector2(1820, -10 + i * 4).RotatedBy(Projectile.rotation) - new Vector2(0, 12 * Projectile.spriteDirection).RotatedBy(Projectile.rotation), DustID.DungeonSpirit, Vector2.Zero, 1, Color.White, 1.5f);
+                    dust.noGravity = true;
+                }
+            }
+
         }
 
         public override bool PreDraw(ref Color lightColor)
@@ -84,36 +138,46 @@ namespace Terrafirma.Projectiles.Summons
             
 
             Main.EntitySpriteDraw(SentryBase, Projectile.Bottom - Main.screenPosition , new Rectangle(0,40,80,34), lightColor, 0, new Vector2(32, 35), 1f, SpriteEffects.None, 0f);
-            Main.EntitySpriteDraw(SentryBase, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), lightColor, Projectile.ai[1], new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 10), 1f, Projectile.spriteDirection == 1? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
-            Main.EntitySpriteDraw(SentryGlow, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), new Color(1f, 1f, 1f, 0) * 0.2f, Projectile.ai[1], new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 8), 1f + Math.Abs((float)Math.Sin(Main.timeForVisualEffects / 40f)) / 20f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
+            Main.EntitySpriteDraw(SentryBase, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), lightColor, Projectile.rotation, new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 10), 1f, Projectile.spriteDirection == 1? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
+            Main.EntitySpriteDraw(SentryGlow, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), new Color(1f, 1f, 1f, 0) * 0.2f, Projectile.rotation, new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 10), 1f + Math.Abs((float)Math.Sin(Main.timeForVisualEffects / 40f)) / 20f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
             ChargeUpFloat = Math.Clamp(Projectile.ai[0] / 120f, 0f, 1f);
-            Main.EntitySpriteDraw(SentryGlow, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), new Color(1f,1f,1f,0) * ChargeUpFloat, Projectile.ai[1], new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 8), 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
+            Main.EntitySpriteDraw(SentryGlow, Projectile.Center - Main.screenPosition + new Vector2(0, 5), new Rectangle(0, 0, 80, 38), new Color(1f,1f,1f,0) * ChargeUpFloat, Projectile.rotation, new Vector2(32, Projectile.spriteDirection == 1 ? 28 : 10), 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
 
             ChargeUpFloat = MathHelper.Lerp(0f, 1f, Math.Clamp((Projectile.ai[0] - 120f) / 480f, 0f, 1f));
             Main.EntitySpriteDraw(SentryLaser, 
-                Projectile.Center - Main.screenPosition + new Vector2(0, 11 * -Projectile.spriteDirection).RotatedBy(Projectile.ai[1]) + new Vector2(0,5), 
+                Projectile.Center - Main.screenPosition + new Vector2(0, 11 * -Projectile.spriteDirection).RotatedBy(Projectile.rotation) + new Vector2(0,5), 
                 SentryLaser.Bounds, 
                 new Color(1f, 1f, 1f, 0) * ChargeUpFloat,
-                Projectile.ai[1], 
+                Projectile.rotation, 
                 new Vector2(-0.23f, SentryLaser.Height / 2), 
                 new Vector2(100f, ChargeUpFloat), 
                 SpriteEffects.None, 
                 0f);
             Main.EntitySpriteDraw(SentryLaser,
-                Projectile.Center - Main.screenPosition + new Vector2(0, 11 * -Projectile.spriteDirection).RotatedBy(Projectile.ai[1]) + new Vector2(0, 5),
+                Projectile.Center - Main.screenPosition + new Vector2(0, 11 * -Projectile.spriteDirection).RotatedBy(Projectile.rotation) + new Vector2(0, 5),
                 SentryLaser.Bounds,
                 new Color(1f, 1f, 1f, 0) * (ChargeUpFloat / 3f),
-                Projectile.ai[1],
+                Projectile.rotation,
                 new Vector2(-0.23f, SentryLaser.Height / 2),
                 new Vector2(100f, ChargeUpFloat + Math.Abs((float)Math.Sin(Main.timeForVisualEffects / 10f)) / 3f),
                 SpriteEffects.None,
                 0f);
+            Main.EntitySpriteDraw(SentryLaser,
+                Projectile.Center - Main.screenPosition + new Vector2(0, 11 * -Projectile.spriteDirection).RotatedBy(Projectile.rotation) + new Vector2(0, 5),
+                SentryLaser.Bounds,
+                new Color(1f, 1f, 1f, 0) * (ChargeUpFloat / 10f),
+                Projectile.rotation,
+                new Vector2(-0.23f, SentryLaser.Height / 2),
+                new Vector2(100f, ChargeUpFloat + Math.Abs((float)Math.Sin(Main.timeForVisualEffects / 20f)) / 2f),
+                SpriteEffects.None,
+                0f);
+
 
             Main.EntitySpriteDraw(SentryLaserBase,
                 Projectile.Center - Main.screenPosition + new Vector2(0, 5),
                 new Rectangle(0, ((int)(Main.timeForVisualEffects / 4f ) % 4) * SentryLaserBase.Height / 4, SentryLaserBase.Width, SentryLaserBase.Height / 4),
                 new Color(1f, 1f, 1f, 0) * ChargeUpFloat,
-                Projectile.ai[1],
+                Projectile.rotation,
                 new Vector2(-20 / ChargeUpFloat, Projectile.spriteDirection == 1 ? 52 : 26),
                 ChargeUpFloat,
                 SpriteEffects.None,
