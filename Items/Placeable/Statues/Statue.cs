7bb1bc920bc0be4feaf09e08d49aef4a199f82a2
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-04-27 17:06:01 -0500

    statue

---
+++ Items/Placeable/Statues/Statue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terrafirma.Tiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Terrafirma.Items.Placeable.Statues
+{
+    public class StrangeBulbStatue : ModItem
+    {
+        public virtual int style => 0;
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.SlimeStatue);
+            Item.createTile = ModContent.TileType<Tiles.Statues>();
+            Item.placeStyle = style;
+        }
+    }
+    public class ExcaliburStatue : StrangeBulbStatue
+    {
+        public override int style => 1;
+    }
+    public class CrossStatue : StrangeBulbStatue
+    {
+        public override int style => 2;
+    }
+    public class OverseeingStatue : StrangeBulbStatue
+    {
+        public override int style => 3;
+    }
+    public class FourPointedStarStatue : StrangeBulbStatue
+    {
+        public override int style => 4;
+    }
+    public class WrenchStatue : StrangeBulbStatue
+    {
+        public override int style => 5;
+    }
+    public class JewelStatue : StrangeBulbStatue
+    {
+        public override int style => 6;
+    }
+}
---
+++ Tiles/Statues.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Terrafirma.Tiles
+{
+    public class Statues : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleWrapLimit = 7;
+            TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+            TileObjectData.addAlternate(TileObjectData.newTile.StyleWrapLimit);
+            TileObjectData.addTile(Type);
+            DustType = DustID.Stone;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            AddMapEntry(new Color(144, 148, 144), Language.GetText("MapObject.Statue"));
+        }
+    }
+}
