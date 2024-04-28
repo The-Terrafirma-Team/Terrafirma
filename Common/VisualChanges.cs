b1c8d40d0e881b43bb5b545dde9339fa826871b8
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-04-27 20:09:53 -0500

    swords

--- Common/VisualChanges.cs
+++ Common/VisualChanges.cs
@@ -12,6 +12,10 @@ namespace Terrafirma.Common
         {
             TextureAssets.Item[ItemID.DemonScythe] = ModContent.Request<Texture2D>(AssetFolder + "ShadowCodex");
             TextureAssets.Item[ItemID.Vilethorn] = ModContent.Request<Texture2D>(AssetFolder + "VileStaff");
+            TextureAssets.Item[ItemID.GoldBroadsword] = ModContent.Request<Texture2D>(AssetFolder + "GoldSword");
+            TextureAssets.Item[ItemID.PlatinumBroadsword] = ModContent.Request<Texture2D>(AssetFolder + "PlatinumSword");
+            TextureAssets.Item[ItemID.SilverBroadsword] = ModContent.Request<Texture2D>(AssetFolder + "SilverSword");
+            TextureAssets.Item[ItemID.TungstenBroadsword] = ModContent.Request<Texture2D>(AssetFolder + "TungstenSword");
         }
     }
 }
