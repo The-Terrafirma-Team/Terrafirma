01b415d33426ca4d8d30035b844f69eb9b8a266f
Jacob Jacobsphere <geometricwarrior@gmail.com>
2024-06-14 01:13:00 -0500

    jeo moment

--- Common/Keybinds.cs
+++ Common/Keybinds.cs
@@ -8,6 +8,16 @@ namespace Terrafirma.Common
 {
     internal class Keybinds : ModPlayer
     {
+        public ModKeybind tertiaryAttack { get; set; }
+        public override void Load()
+        {
+            tertiaryAttack = KeybindLoader.RegisterKeybind(Mod, "tertiaryAttack", Keys.C);
+        }
+        public override void Unload()
+        {
+            tertiaryAttack = null;
+        }
+
         public bool Shifting;
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
