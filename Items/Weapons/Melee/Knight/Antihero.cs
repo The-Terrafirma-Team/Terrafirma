d31dba28765b7ce33dce290f2137f7eccadc06ae
Fred-Leon Brrandibur <60478514+ItzFred@users.noreply.github.com>
2025-01-13 22:42:37 +0100

    Antihero

---
+++ Items/Weapons/Melee/Knight/Antihero.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Linq;
+using Terrafirma.Items.Materials;
+using Terrafirma.Projectiles.Melee.Knight;
+using Terrafirma.Rarities;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Terrafirma.Items.Weapons.Melee.Knight
+{
+    public class Antihero : ModItem
+    {
+        Projectile swordproj = null;
+        public float auraPresence = 0f;
+        public float auraRadius = -1;
+        public int auraFadeTimer = 0;
+        private int damageTick = 4;
+        public override void SetDefaults()
+        {
+            Item.DefaultToSword(75, 10, 3);
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 52;
+            Item.height = 56;
+            Item.UseSound = new SoundStyle("Terrafirma/Sounds/SwordSound2") { PitchVariance = 0.3f, Pitch = -0.45f, MaxInstances = 10 };
+            Item.shoot = ModContent.ProjectileType<AntiheroProjectile>();
+            Item.shootSpeed = 8;
+            Item.rare = ModContent.RarityType<FinalQuestRarity>();
+            Item.value = Item.sellPrice(0, 10, 0, 0);
+            Item.shootSpeed = 16f;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Asset<Texture2D> glowtex = ModContent.Request<Texture2D>("Terrafirma/Items/Weapons/Melee/Knight/AntiheroGlow");
+
+            spriteBatch.Draw(glowtex.Value,
+                Item.position + Item.Size / 2 - Main.screenPosition,
+                glowtex.Frame(),
+                new Color(1f,0f,0f,0f) * (float)((Math.Sin(Main.timeForVisualEffects / 10f) + 1.5f) / 5f),
+                rotation,
+                Item.Size / 2 + new Vector2(-10,0),
+                scale + (float)(Math.Sin(Main.timeForVisualEffects / 10f) + 1f) / 10f,
+                SpriteEffects.None,
+                1f);
+            return base.PreDrawInWorld(spriteBatch, lightColor, alphaColor, ref rotation, ref scale, whoAmI);
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Asset<Texture2D> glowtex = ModContent.Request<Texture2D>("Terrafirma/Items/Weapons/Melee/Knight/AntiheroGlow");
+
+            spriteBatch.Draw(glowtex.Value,
+                position,
+                glowtex.Frame(),
+                new Color(1f, 0f, 0f, 0f) * (float)((Math.Sin(Main.timeForVisualEffects / 10f) + 1.5f) / 5f),
+                0f,
+                Item.Size / 2 + new Vector2(-10, 0),
+                scale + (float)(Math.Sin(Main.timeForVisualEffects / 10f) + 1f) / 20f,
+                SpriteEffects.None,
+                1f);
+            return base.PreDrawInInventory(spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return (player.CheckTension(20,false) || player.ownedProjectileCounts[ModContent.ProjectileType<AntiheroProjectile>()] > 0);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                if (player.ownedProjectileCounts[ModContent.ProjectileType<AntiheroProjectile>()] > 0)
+                {
+                    Item.noUseGraphic = false;
+                    if (swordproj != null) swordproj.Kill();
+                }
+                else
+                {
+                    Item.noUseGraphic = true;
+                    swordproj = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, player.whoAmI);
+                    swordproj.scale = Item.scale;
+                    swordproj.Size *= Item.scale;
+                    player.CheckTension(20, true);
+                }
+            }
+            else
+            {
+                Item.noUseGraphic = false;
+                if (swordproj != null) swordproj.Kill();
+                
+            }
+            return false;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<AntiheroProjectile>()] > 0) auraRadius = -1;
+            else auraRadius = MathHelper.Lerp(auraRadius, 200, 0.15f);
+
+            if (auraFadeTimer == 0) auraPresence = MathHelper.Clamp(auraPresence - 0.05f, 0f, 1f);
+            auraFadeTimer = (int)MathHelper.Clamp(auraFadeTimer - 1, 0, 10000);
+
+            base.HoldItem(player);
+        }
+        public override void UseAnimation(Player player)
+        {
+            base.UseAnimation(player);
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            damageTick--;
+            if (player.altFunctionUse != 2)
+            {
+                auraPresence = 1f;
+                auraFadeTimer = 5;
+            }
+            NPC[] npcArray = new NPC[] { };
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                if (Main.npc[i].Center.Distance(player.Center) <= auraRadius && Main.npc[i].active)
+                {
+                    npcArray = npcArray.Append(Main.npc[i]).ToArray();
+                }
+            }
+            for (int i = 0; i < npcArray.Length; i++)
+            {
+                if (i % 4 == damageTick)
+                {
+                    player.ApplyDamageToNPC(npcArray[i], (int)(Item.damage * 0.2f), 0f, npcArray[i].Center.X > player.Center.X ? 1 : -1, damageType: DamageClass.Melee);
+
+                    for (int k = 0; k < 5; k++)
+                    {
+                        Vector2 vel = Main.rand.NextVector2Circular(4f, 4f);
+                        Dust.NewDust(npcArray[i].Center, 4, 4, DustID.Blood, vel.X, vel.Y, Scale: 1.5f);
+                    }
+
+                    player.GiveTension(3);
+                }
+            }
+            if (damageTick <= 0) damageTick = 4;
+            return base.UseItem(player);
+        }
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Vector2 vel = Main.rand.NextVector2Circular(4f, 4f);
+                Dust.NewDust(target.Center, 4, 4, DustID.Blood, vel.X, vel.Y, Scale: 1.5f);
+            }
+            base.OnHitNPC(player, target, hit, damageDone);
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<AntiheroProjectile>()] > 0 && swordproj != null)
+            {
+                swordproj.ai[1] = 1;
+            }
+            return player.ownedProjectileCounts[ModContent.ProjectileType<AntiheroProjectile>()] <= 0;
+        }
+
+    }
+}
---
+++ Projectiles/Melee/Knight/AntiheroProjectile.cs
@@ -0,0 +1,170 @@
+﻿using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+using System;
+using Terrafirma.Particles;
+using Terrafirma.Items.Weapons.Melee.Knight;
+using Terraria.GameContent;
+using ReLogic.Content;
+using System.Collections.Generic;
+
+namespace Terrafirma.Projectiles.Melee.Knight
+{
+    public class AntiheroProjectile : ModProjectile
+    {
+        NPC targetNPC = null;
+        Vector2 targetOffset = Vector2.Zero;
+        public override string Texture => "Terrafirma/Projectiles/Melee/Knight/AntiheroProjectile";
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 4;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
+        }
+
+        bool returnbool = false;
+        public override void SetDefaults()
+        {          
+            Projectile.width = 40;
+            Projectile.height = 40;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 10000;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.extraUpdates = 2;
+            Projectile.hide = true;
+        }
+        public override void AI()
+        {
+            if (Projectile.velocity != Vector2.Zero) Projectile.rotation = MathHelper.Lerp(Projectile.rotation, Projectile.velocity.ToRotation(), 0.2f);
+            if (Projectile.ai[1] == 1)
+            {
+                Projectile.tileCollide = false;
+                Projectile.spriteDirection = -1;              
+                Projectile.velocity = Vector2.Lerp(Projectile.velocity, Projectile.Center.DirectionTo(Main.player[Projectile.owner].Center) * 16f, 0.04f);                
+                targetNPC = null;
+                if (Main.player[Projectile.owner].Center.Distance(Projectile.Center) <= 50) Projectile.Kill();
+            }
+
+            if (Main.player[Projectile.owner].Center.Distance(Projectile.Center) > 600f) Projectile.ai[1] = 1;
+            if (Main.player[Projectile.owner].Center.Distance(Projectile.Center) > 900f) Projectile.Kill();
+
+            if (targetNPC != null)
+            {
+                Projectile.Center = targetNPC.Center + targetOffset.RotatedBy(targetNPC.rotation);
+                Projectile.rotation = Projectile.Center.AngleTo(targetNPC.Center);
+
+                if (targetNPC.immune[Projectile.owner] == 0)
+                {
+                    Main.player[Projectile.owner].ApplyDamageToNPC(targetNPC, Projectile.damage, 0f, 1, damageType: DamageClass.Melee);
+                    targetNPC.immune[Projectile.owner] = 20;
+
+                    int healAmount = (int)(4 * (((600 - (int)Main.player[Projectile.owner].Center.Distance(Projectile.Center)) / 300f) + 1));
+
+                    if (healAmount > 0 && targetNPC.type != NPCID.TargetDummy) Main.player[Projectile.owner].Heal(healAmount);
+
+                    for (int i = 0; i < Projectile.Center.Distance(Main.player[Projectile.owner].Center); i += 5)
+                    {
+                        float dist = i / Projectile.Center.Distance(Main.player[Projectile.owner].Center);
+                        Vector2 pos = new Vector2(
+                            MathHelper.Lerp(Projectile.Center.X, Main.player[Projectile.owner].Center.X, dist),
+                            MathHelper.Lerp(Projectile.Center.Y, Main.player[Projectile.owner].Center.Y, dist * dist)
+                        );
+                        Dust d = Dust.NewDustPerfect(pos, DustID.LifeDrain, Vector2.Zero);
+                        d.noGravity = true;
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Vector2 vel = Main.rand.NextVector2Circular(4f, 4f);
+                        Dust.NewDust(Projectile.Center + Projectile.Center.DirectionTo(targetNPC.Center) * 40, 4, 4, DustID.Blood, vel.X, vel.Y, Scale: 1.5f);
+                    }
+
+                }
+                if (!targetNPC.active) Projectile.ai[1] = 1;
+            }
+
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Projectile.ai[1] == 0) Projectile.velocity = Vector2.Zero;
+            if (targetNPC == null || !targetNPC.active)
+            {
+                targetNPC = target;
+                targetOffset = (Projectile.Center - target.Center).RotatedBy(-targetNPC.rotation);
+            }
+            base.OnHitNPC(target, hit, damageDone);
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            if (targetNPC == null)
+            {
+                return base.CanHitNPC(target);
+            }
+            else if (target == targetNPC)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.ai[1] = 1;
+            return false;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            if (Main.player[Projectile.owner].HeldItem.ModItem is Antihero item )
+            {
+                item.auraPresence = 1f;
+                item.auraFadeTimer = 30;
+            }
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCsAndTiles.Add(index);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D SwordTexture = TextureAssets.Projectile[Type].Value;
+            for (int i = 0; i < Projectile.oldPos.Length; i+=2)
+            {
+                Main.EntitySpriteDraw(SwordTexture,
+                    (Projectile.oldPos[i] + Projectile.Size/2f) - Main.screenPosition,
+                    SwordTexture.Frame(),
+                    new Color(1f,0f,0f,0f) * (1f - (i / (float)Projectile.oldPos.Length)) * 0.5f,
+                    Projectile.rotation + MathHelper.PiOver4 * Projectile.spriteDirection,
+                    SwordTexture.Size() / 2,
+                    Projectile.scale + (float)((Math.Sin(Main.timeForVisualEffects / 10f) + 1f) / 10f),
+                    Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+            }
+            Main.EntitySpriteDraw(SwordTexture,
+                Projectile.Center - Main.screenPosition,
+                SwordTexture.Frame(),
+                lightColor,
+                Projectile.rotation + MathHelper.PiOver4 * Projectile.spriteDirection,
+                SwordTexture.Size() / 2,
+                Projectile.scale,
+                Projectile.spriteDirection == 1? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+            Main.EntitySpriteDraw(SwordTexture,
+                Projectile.Center - Main.screenPosition,
+                SwordTexture.Frame(),
+                new Color(1f, 1f, 1f, 0f) * 0.5f,
+                Projectile.rotation + MathHelper.PiOver4 * Projectile.spriteDirection,
+                SwordTexture.Size() / 2,
+                Projectile.scale + (float)((Math.Sin(Main.timeForVisualEffects / 10f) + 1f) / 10f),
+                Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+
+            return false;
+        }
+
+    }
+}
