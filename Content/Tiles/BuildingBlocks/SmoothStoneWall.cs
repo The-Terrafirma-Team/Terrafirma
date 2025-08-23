76748aa7032ead53c258ac8fa417536926f4e1ce
Jacob Jacobsphere <geometricwarrior@gmail.com>
2025-08-22 22:46:23 -0500

    smooth stone wall

---
+++ Content/Tiles/BuildingBlocks/SmoothStoneWall.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Terrafirma.Content.Tiles.BuildingBlocks
+{
+    public class SmoothStoneWallItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.StoneWall);
+            Item.createWall = ModContent.WallType<SmoothStoneWall>();
+            Item.placeStyle = 0;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(4).AddTile(TileID.WorkBenches).AddIngredient(ModContent.ItemType<SmoothStone>(), 1).Register();
+            Recipe.Create(ModContent.ItemType<SmoothStone>()).AddTile(TileID.WorkBenches).AddIngredient(Type, 4).Register();
+        }
+    }
+    public class SmoothStoneWall : ModWall
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.wallHouse[Type] = true;
+
+            DustType = DustID.Stone;
+
+            AddMapEntry(new Color(56, 56, 56));
+        }
+    }
+}
